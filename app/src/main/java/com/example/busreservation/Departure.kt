ff1573a910238c47a6d56aa11a5fbea83b8612e3
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2021-11-09 00:13:50 +0900

    유재만 : 출발지 , 목적지 선택 화면 작업

---
+++ app/src/main/java/com/example/busreservation/Departure.kt
@@ -0,0 +1,11 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Departure : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_departure)
+    }
+}
---
+++ app/src/main/java/com/example/busreservation/Find_Path.kt
@@ -0,0 +1,13 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Find_Path : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_path)
+
+
+    }
+}
