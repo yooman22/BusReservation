b9acb4489225118deb3464be5cd808bb894cac14
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2021-11-24 14:13:24 +0900

    유재만 : 목적지 db 데이터로 뿌려지도록 작업

--- app/src/main/java/com/example/busreservation/Destination.kt
+++ app/src/main/java/com/example/busreservation/Destination.kt
@@ -1,24 +1,68 @@
 package com.example.busreservation
 
+import Network.RetrofitClient
+import Network.RetrofitService
+import VO.ListVO
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_departure.*
 import kotlinx.android.synthetic.main.activity_destination.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Destination : AppCompatActivity() {
+
+    var destination_txt : ArrayList<String> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_destination)
 
-        val destination_txt = arrayOf("울산대학교앞", "울산대학교후문", "현대청운고앞","현대중공업일산문","천상1교사거리","천상중학교")
+        //val destination_txt = arrayOf("울산대학교앞", "울산대학교후문", "현대청운고앞","현대중공업일산문","천상1교사거리","천상중학교")
+
+        //val adapter_destination = ArrayAdapter(this, android.R.layout.simple_list_item_1, destination_txt)
+
+        //destination_list.setAdapter(adapter_destination)
+
+        var list = ArrayList<String>()
+
+        var result : ListVO = ListVO(list)
 
         val adapter_destination = ArrayAdapter(this, android.R.layout.simple_list_item_1, destination_txt)
 
-        destination_list.setAdapter(adapter_destination)
+        val api = RetrofitClient.getInstance().create(RetrofitService::class.java)
+        val callGetLogin = api.API_List("1")
+            .enqueue(object : Callback<ListVO> {
+                override fun onResponse(call: Call<ListVO>, response: Response<ListVO>) {
+                    Log.d("결과", "성공 : ${response.raw()}")
+                    Log.d("결과", "성공 : ${response.body()?.result}")
+                    response.body()?.let {
+                        result = it
+
+                        for(i in result.result) {
+                            destination_txt.add(i)
+                            Log.d("결과", "성공 : " + i)
+                            adapter_destination.add(i)
+                        }
+                        destination_list.setAdapter(adapter_destination)
+
+                    }
+
+                }
+                override fun onFailure(call: Call<ListVO>, t: Throwable) {
+
+                    Log.d("결과:", "실패 : $t")
+                }
+            })
+
+
+
 
         destination_list.onItemClickListener = object : AdapterView.OnItemClickListener {
             override fun onItemClick(parent: AdapterView<*>, v: View, position: Int, id: Long) {
