2eea8b0229f75013add51917bcb3ecdf5a6ed8b2
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-03-05 14:26:45 +0900

    유재만 : 강아지 페이지 작업

---
+++ app/src/main/java/com/example/busreservation/DogPage.kt
@@ -0,0 +1,15 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DogPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_page)
+    }
+
+
+
+}
