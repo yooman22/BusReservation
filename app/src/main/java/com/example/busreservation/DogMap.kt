c0f73e967e8adf6a454a3f3ed7723d280765edfe
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-04-03 12:39:42 +0900

    유재만 : 지도 화면 추가 API 추후에 추가 예정

---
+++ app/src/main/java/com/example/busreservation/DogMap.kt
@@ -0,0 +1,11 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DogMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_map)
+    }
+}
