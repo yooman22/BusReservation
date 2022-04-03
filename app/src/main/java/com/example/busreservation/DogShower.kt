f829ec77de803022ec819ccfc21ffe39f650f78f
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-04-03 12:44:45 +0900

    유재만 : 샤워 화면 개발

---
+++ app/src/main/java/com/example/busreservation/DogShower.kt
@@ -0,0 +1,11 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DogShower : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_shower)
+    }
+}
