d5d59495652f1dda96226236198c14f144fc9ea9
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-04-07 15:59:54 +0900

    유재만 : 사료 소개 화면 완료

---
+++ app/src/main/java/com/example/busreservation/DogFeedItemInfo.kt
@@ -0,0 +1,11 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DogFeedItemInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_feed_item_info)
+    }
+}
