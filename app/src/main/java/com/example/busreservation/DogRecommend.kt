bf5af719595c1eadc895b44da895015fd8d2b44f
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-04-09 22:08:50 +0900

    유재만 : 강아지 사료 리스트 연결

--- app/src/main/java/com/example/busreservation/DogRecommend.kt
+++ app/src/main/java/com/example/busreservation/DogRecommend.kt
@@ -1,11 +1,19 @@
 package com.example.busreservation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dog_recommend.*
 
 class DogRecommend : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog_recommend)
+
+        confirm_recommand.setOnClickListener {
+            val nextIntent = Intent(this, DogFeedList::class.java)
+            startActivity(nextIntent)
+        }
+
     }
 }
