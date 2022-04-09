e44a5ff9abbc1b1fc97d62212a77fa2c9f4eba83
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-04-09 17:16:17 +0900

    유재만 : 강아지 몸무게 저장 로직 추가

--- app/src/main/java/com/example/busreservation/DogWeight.kt
+++ app/src/main/java/com/example/busreservation/DogWeight.kt
@@ -2,10 +2,64 @@ package com.example.busreservation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_dog_registration_two.*
+import kotlinx.android.synthetic.main.activity_dog_weight.*
 
 class DogWeight : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog_weight)
+
+        btn_A.setOnClickListener {
+            calc_View.visibility = View.VISIBLE
+            user_View.visibility = View.GONE
+
+            btn_A.background = getResources().getDrawable(R.drawable.edge_green_btn)
+            btn_B.background = getResources().getDrawable(R.drawable.edge)
+
+        }
+
+        btn_B.setOnClickListener {
+            calc_View.visibility = View.GONE
+            user_View.visibility = View.VISIBLE
+
+            btn_A.background = getResources().getDrawable(R.drawable.edge)
+            btn_B.background = getResources().getDrawable(R.drawable.edge_green_btn)
+
+        }
+
+        confirm_weight.setOnClickListener {
+
+            var weight_dog = edit_A.text.toString().toLong() - edit_B.text.toString().toLong()
+
+            if(calc_View.visibility == View.VISIBLE) {
+
+                if (weight_dog < 0 || edit_A.text.toString().isNullOrEmpty() ||  edit_B.text.toString().isNullOrEmpty()  ) {
+                    Toast.makeText(this, "잘못된 입력 입니다.", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
+                SharedPreference.prefs.setString("dog_weight",weight_dog.toString())
+
+            }
+
+            if(user_View.visibility == View.VISIBLE){
+
+                if (edit_C.text.toString().isNullOrEmpty() ) {
+                    Toast.makeText(this, "잘못된 입력 입니다.", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+
+                SharedPreference.prefs.setString("dog_weight",edit_C.text.toString())
+
+            }
+
+
+
+        }
+
+
     }
 }
