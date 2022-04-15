132ada6e6768b10baef437840972f2ad5e85bf74
DESKTOP-31K2ECJ\YuJaeMan <yooman22@naver.com>
2022-04-15 21:53:17 +0900

    유재만 : 강아지 좋아 하는 것 , 싫어 하는 것 화면 개발 및 기능 개발

---
+++ app/src/main/java/com/example/busreservation/DogProtein.kt
@@ -0,0 +1,257 @@
+package com.example.busreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_dog_protein.*
+
+class DogProtein : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_protein)
+
+        var flag = false
+
+        var dogScreenList_ : MutableList<Boolean> = arrayListOf()
+        var dogTypeList_ : MutableList<Int> = arrayListOf()
+
+        var favor_Count = 0
+        var hate_Count = 0
+
+        for(i : Int in 0 .. 9) {
+            dogScreenList_.add(intent.getBooleanExtra("btn"+i+1, false))
+            var type = intent.getIntExtra("type"+i+1,-1)
+            if(type == 0){
+                hate_Count++
+            }
+            else if(type == 1){
+                favor_Count++
+            }
+            dogTypeList_.add(type)
+        }
+
+
+
+
+        var favorHateFlag = intent.getIntExtra("fh",-1)
+
+        if(favorHateFlag == 0){ // 싫어하는
+            title_.setText("싫어하는 단백질원")
+            flag = false
+        }else { // 좋아하는
+            title_.setText("좋아하는 단백질원")
+            flag = true
+        }
+        
+        btn_food1.setOnClickListener {
+            if(dogScreenList_[0]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[0] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[0] = 1
+                btn_food1.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[0] = 0
+                btn_food1.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+
+        }
+
+        btn_food2.setOnClickListener {
+            if(dogScreenList_[1]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[1] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[1] = 1
+                btn_food2.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[1] = 0
+                btn_food2.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food3.setOnClickListener {
+            if(dogScreenList_[2]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[2] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[2] = 1
+                btn_food3.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[2] = 0
+                btn_food3.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food4.setOnClickListener {
+            if(dogScreenList_[3]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[3] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[3] = 1
+                btn_food4.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[3] = 0
+                btn_food4.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food5.setOnClickListener {
+            if(dogScreenList_[4]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[4] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[4] = 1
+                btn_food5.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[4] = 0
+                btn_food5.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food6.setOnClickListener {
+            if(dogScreenList_[5]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[5] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[5] = 1
+                btn_food6.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[5] = 0
+                btn_food6.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food7.setOnClickListener {
+            if(dogScreenList_[6]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[6] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[6] = 1
+                btn_food7.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[6] = 0
+                btn_food7.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food8.setOnClickListener {
+            if(dogScreenList_[7]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[7] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[7] = 1
+                btn_food8.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[7] = 0
+                btn_food8.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food9.setOnClickListener {
+            if(dogScreenList_[8]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[8] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[8] = 1
+                btn_food9.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[8] = 0
+                btn_food9.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        btn_food10.setOnClickListener {
+            if(dogScreenList_[9]|| favor_Count == 4 || hate_Count == 4){
+                Toast.makeText(this,"선택이 불가능 합니다.",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            dogScreenList_[9] = true
+
+            if(flag)
+            {
+                favor_Count++
+                dogTypeList_[9] = 1
+                btn_food10.setBackgroundResource(R.drawable.edge_green_btn)
+            }else {
+                hate_Count++
+                dogTypeList_[9] = 0
+                btn_food10.setBackgroundResource(R.drawable.edge_red_btn)
+            }
+        }
+
+        confirm.setOnClickListener {
+            for(i : Int in 0 .. 9) {
+                intent.putExtra("btn"+i+1,false)
+                intent.putExtra("type"+i+1,-1)
+            }
+            finish()
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/busreservation/DogRecommend.kt
+++ app/src/main/java/com/example/busreservation/DogRecommend.kt
@@ -1,19 +1,260 @@
 package com.example.busreservation
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_dog_recommend.*
+import kotlinx.android.synthetic.main.activity_dog_registration_one.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DogRecommend : AppCompatActivity() {
+
+    var dogProteinList : MutableList<DogProteinItem> = arrayListOf()
+
+    var dogFavorList : MutableList<DogProteinItem> = arrayListOf()
+    var dogHateList : MutableList<DogProteinItem> = arrayListOf()
+
+    var favorCount = 0
+    var hateCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dog_recommend)
+
+        dogProteinList.add(DogProteinItem("소고기",false,-1))
+        dogProteinList.add(DogProteinItem("닭고기",false,-1))
+        dogProteinList.add(DogProteinItem("생선",false,-1))
+        dogProteinList.add(DogProteinItem("칠면조",false,-1))
+        dogProteinList.add(DogProteinItem("양고기",false,-1))
+        dogProteinList.add(DogProteinItem("돼지고기",false,-1))
+        dogProteinList.add(DogProteinItem("오리고기",false,-1))
+        dogProteinList.add(DogProteinItem("사슴고기",false,-1))
+        dogProteinList.add(DogProteinItem("토끼고기",false,-1))
+        dogProteinList.add(DogProteinItem("식물성 단백질",false,-1))
 
         confirm_recommand.setOnClickListener {
             val nextIntent = Intent(this, DogFeedList::class.java)
             startActivity(nextIntent)
         }
 
+        val startForResult_favor = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result: ActivityResult -> if (result.resultCode == Activity.RESULT_OK) {
+
+            val intent = result.data!! // Handle the Intent //do stuff here
+            for(i : Int in 0 .. 9) {
+                dogProteinList[i].type = intent.getIntExtra("type"+i+1, -1)
+                dogProteinList[i].Flag = intent.getBooleanExtra("btn"+i+1, false)
+
+                if(dogProteinList[i].type == 0)
+                {
+                    dogHateList.add(dogFavorList[i])
+                    hateCount++
+                }
+                else if(dogProteinList[i].type == 1)
+                {
+                    dogFavorList.add(dogFavorList[i])
+                    favorCount++
+                }
+
+            }
+
+            favorView(false,0)
+
+            var count = dogFavorList.count()
+
+            if(count !=0){
+                favorView(true,count)
+                setFavorView(count,dogFavorList)
+            }
+
+
+
+
+            }
+        }
+
+        favor1.setOnClickListener{
+            val nextIntent = Intent(this, DogProtein::class.java)
+            nextIntent.putExtra("fh",1)
+            for(i : Int in 0 .. 9){
+                nextIntent.putExtra("btn"+i+1,dogProteinList[i].Flag)
+                nextIntent.putExtra("type"+i+1,dogProteinList[i].type)
+            }
+            startForResult_favor.launch(nextIntent)
+
+
+        }
+
+        val startForResult_hate = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result: ActivityResult -> if (result.resultCode == Activity.RESULT_OK) {
+
+            val intent = result.data!! // Handle the Intent //do stuff here
+            for(i : Int in 0 .. 9) {
+                dogProteinList[i].type = intent.getIntExtra("type"+i+1, -1)
+                dogProteinList[i].Flag = intent.getBooleanExtra("btn"+i+1, false)
+
+                if(dogProteinList[i].type == 0)
+                {
+                    dogHateList.add(dogFavorList[i])
+                    hateCount++
+                }
+                else if(dogProteinList[i].type == 1)
+                {
+                    dogFavorList.add(dogFavorList[i])
+                    favorCount++
+                }
+            }
+
+            hateView(false,0)
+
+            var count = dogHateList.count()
+
+            if(count !=0){
+                hateView(true,count)
+                setHateView(count,dogHateList)
+            }
+
+
+            }
+        }
+
+        hate1.setOnClickListener{
+            val nextIntent = Intent(this, DogProtein::class.java)
+            nextIntent.putExtra("fh",0)
+            for(i : Int in 0 .. 9){
+                nextIntent.putExtra("btn"+i+1,dogProteinList[i].Flag)
+                nextIntent.putExtra("type"+i+1,dogProteinList[i].type)
+            }
+            startForResult_hate.launch(nextIntent)
+        }
+
     }
+
+
+    fun hateView(flag : Boolean,Count : Int){
+        if(flag)
+        {
+            when(Count)
+            {
+                1-> hate1.visibility = View.VISIBLE
+
+                2-> {
+                    hate1.visibility = View.VISIBLE
+                    hate2.visibility = View.VISIBLE
+                }
+                3-> {
+                    hate1.visibility = View.VISIBLE
+                    hate2.visibility = View.VISIBLE
+                    hate3.visibility = View.VISIBLE
+                }
+                4-> {
+                    hate1.visibility = View.VISIBLE
+                    hate2.visibility = View.VISIBLE
+                    hate3.visibility = View.VISIBLE
+                    hate4.visibility = View.VISIBLE
+                }
+            }
+
+        }
+        else {
+            hate1.visibility = View.INVISIBLE
+            hate2.visibility = View.INVISIBLE
+            hate3.visibility = View.INVISIBLE
+            hate4.visibility = View.INVISIBLE
+
+        }
+    }
+
+    fun setHateView(count: Int,list: MutableList<DogProteinItem>){
+        when(count)
+        {
+            1-> hate1.setText(list[0].title)
+
+            2-> {
+                hate1.setText(list[0].title)
+                hate2.setText(list[1].title)
+            }
+            3-> {
+                hate1.setText(list[0].title)
+                hate2.setText(list[1].title)
+                hate3.setText(list[2].title)
+            }
+            4-> {
+                hate1.setText(list[0].title)
+                hate2.setText(list[1].title)
+                hate3.setText(list[2].title)
+                hate4.setText(list[3].title)
+            }
+        }
+    }
+
+    fun favorView(flag: Boolean,Count : Int){
+        if(flag)
+        {
+            when(Count)
+            {
+                1-> favor1.visibility = View.VISIBLE
+
+                2-> {
+                    favor1.visibility = View.VISIBLE
+                    favor2.visibility = View.VISIBLE
+                }
+                3-> {
+                    favor1.visibility = View.VISIBLE
+                    favor2.visibility = View.VISIBLE
+                    favor3.visibility = View.VISIBLE
+                }
+                4-> {
+                    favor1.visibility = View.VISIBLE
+                    favor2.visibility = View.VISIBLE
+                    favor3.visibility = View.VISIBLE
+                    favor4.visibility = View.VISIBLE
+                }
+            }
+        }
+        else {
+            favor1.visibility = View.INVISIBLE
+            favor2.visibility = View.INVISIBLE
+            favor3.visibility = View.INVISIBLE
+            favor4.visibility = View.INVISIBLE
+        }
+    }
+
+    fun setFavorView(count : Int ,list: MutableList<DogProteinItem> )
+    {
+        when(count)
+        {
+            1-> favor1.setText(list[0].title)
+
+            2-> {
+                favor1.setText(list[0].title)
+                favor2.setText(list[1].title)
+            }
+            3-> {
+                favor1.setText(list[0].title)
+                favor2.setText(list[1].title)
+                favor3.setText(list[2].title)
+            }
+            4-> {
+                favor1.setText(list[0].title)
+                favor2.setText(list[1].title)
+                favor3.setText(list[2].title)
+                favor4.setText(list[3].title)
+            }
+        }
+    }
+
+
 }
+
+data class DogProteinItem(
+    var title : String ,
+    var Flag : Boolean,
+    var type : Int
+)
